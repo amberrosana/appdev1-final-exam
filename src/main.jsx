646e266b5bcae0565150085f8103474fb904e413
amberrosana <amberprincessrosana05@gmail.com>
2025-11-29 10:56:20 +0800

    Install GitHub pages for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App.jsx";
 import { store } from "./app/store";
 
-createRoot(document.getElementById('root')).render(
+const basename = "/appdev1-final-exam";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter basename={basename}>
+        <App />
+      </BrowserRouter>
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
---
+++ src/readME.md
@@ -0,0 +1,2 @@
+## Live Demo
+You can view the live version of the app here:  
